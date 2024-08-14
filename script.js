c277d1ecd253fff32f1d1f3b8cb6e4da3f80e4db
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-08-14 16:53:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let inp = document.querySelector("#fname")
+inp.addEventListener("blur", converted)
+function converted(){
+	let res = inp.value
+	inp.value = res.toUpperCase();
+}
